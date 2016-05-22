bff5f8f30d19d8605ed5242317b969322ff60da6
Bryce Meyer <bryce@axolotlinteractive.com>
2016-05-21 22:37:54 -0500

    added general end point function

--- src/TypeFormClient.php
+++ src/TypeFormClient.php
@@ -2,6 +2,7 @@
 namespace AxolotlInteractive\TypeForm;
 use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use stdClass;
@@ -16,14 +17,30 @@ class TypeFormClient
 {
 
     /**
+     * Helper constant for get requests
+     */
+    const METHOD_GET = 'GET';
+
+    /**
+     * Helper constant for post requests
+     */
+    const METHOD_POST = 'POST';
+
+    /**
+     * Helper constant for put requests
+     */
+    const METHOD_PUT = 'PUT';
+
+    /**
+     * Helper constant for delete requests
+     */
+    const METHOD_DELETE = 'DELETE';
+
+
+    /**
      * @var string The base url for the typeform api
      */
     private $baseURL = "https://api.typeform.io/v";
-
-    /**
-     * @var string the key passed in by the user
-     */
-    private $apiKey;
 
     /**
      * @var Client The guzzle client instance for this TypeForm Client
@@ -41,27 +58,46 @@ class TypeFormClient
         if (!in_array($apiVersion, [0.4]))
             throw new Exception("Type Form api version $apiVersion is not currently supported");
 
-        $this->guzzleClient = new Client(["base_uri", $this->baseURL . $apiVersion]);
+        $options = [
+            "base_uri" => $this->baseURL . $apiVersion,
+            "headers" => [
+                "X-API-TOKEN" => $apiKey
+            ]
+        ];
 
-        $this->apiKey = $apiKey;
+        $this->guzzleClient = new Client($options);
     }
+
 
     /**
      * Sends a get request to the type form api and returns a response
      *
+     * @param string $method the method we are running
      * @param string $endPoint The end point requested
      * @param array $params An array of params for this
      * @return stdClass The response from the server
-     * @throws Exception if the end point failed
+     * @throws Exception If the status code on the response is 403 or the method is not valid
      */
-    public function getEndPoint($endPoint, array $params = []) {
-        $params["key"] = $this->apiKey;
+    public function sendEndPoint($method, $endPoint, array $params = []) {
+        $data = [];
 
-        $data = ["query" => $params];
+        switch ($method) {
+            case static::METHOD_DELETE:
+            case static::METHOD_GET:
+                $data["query"] = $params;
+                break;
+            case static::METHOD_POST:
+            case static::METHOD_PUT:
+                $data["json"] = $params;
+                break;
+            default:
+                throw new Exception("Invalid HTTP method passed into endpoint");
+        }
 
-        $response = $this->guzzleClient->get($endPoint, $data);
+        $response = $this->guzzleClient->request($method, $endPoint, $data);
 
-        $this->validateKey($response);
+        if ($response->getStatusCode() == 403)
+            throw new Exception('The supplied API key is not a valid Type Form API Key.');
 
         $body = $response->getBody();
         $responseData = json_decode($body, true);
@@ -69,14 +105,4 @@ class TypeFormClient
         return $responseData;
     }
 
-    /**
-     * Validates that the response did not receive a 403 status code
-     *
-     * @param ResponseInterface $response A response received from Type Form
-     * @throws Exception If the status code on the response is 403
-     */
-    private function validateKey(ResponseInterface $response) {
-        if ($response->getStatusCode() == 403)
-            throw new Exception('The supplied API key is not a valid Type Form API Key.');
-    }
 }
