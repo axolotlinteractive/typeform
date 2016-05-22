4dbe2570683f3867f91006df13ee111392fb9fb0
Bryce Meyer <bryce@axolotlinteractive.com>
2016-05-21 21:51:03 -0500

    added constructor for typeform client

--- src/TypeFormClient.php
+++ src/TypeFormClient.php
@@ -1,4 +1,7 @@
 <?php
+namespace AxolotlInteractive\TypeForm;
+use Exception;
+use GuzzleHttp\Client;
 
 /**
  * Created by PhpStorm.
@@ -6,7 +9,33 @@
  * Date: 5/21/16
  * Time: 9:30 PM
  */
-class TypeFormClient
+class TypeFormClient extends Client
 {
 
+    /**
+     * @var string The base url for the typeform api
+     */
+    private $baseURL = "https://api.typeform.io/v";
+
+    /**
+     * @var string the key passed in by the user
+     */
+    private $apiKey;
+
+
+    /**
+     * TypeFormClient constructor.
+     * @param double $apiVersion The version of the api as a double
+     * @param string $apiKey The api key for type form
+     * @throws Exception If an incorrect version was passed in
+     */
+    public function __construct($apiVersion, $apiKey)
+    {
+        if (!in_array($apiVersion, [0.4]))
+            throw new Exception("Type Form api version $apiVersion is not currently supported");
+
+        parent::__construct(["base_uri", $this->baseURL . $apiVersion]);
+
+        $this->apiKey = $apiKey;
+    }
 }
